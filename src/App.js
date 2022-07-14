d3240d51e17d61b49763f28875c65b960aa4d979
AlejandroBojaca <alejandrobojaca1@gmail.com>
2022-07-14 21:25:47 +0300

    👆 Added Selection screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import {Button, Container} from '@mui/material'
+import styled from 'styled-components'
+import "./button-styles.css"
+
+const StyledButton = styled(Button)`
+  width: 50vw;
+  height: 100vh;
+  border-radius:0;
+`
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  flex-grow:1;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>ProgresivePomodoro</h1>
-      </header>
+        <StyledContainer maxWidth="fixed" disableGutters>
+          <StyledButton variant="contained" className="no-border-radius" style={{borderRadius:0}}>Classic</StyledButton>
+          <StyledButton variant="contained" color="success" className="no-border-radius-radius" style={{borderRadius:0}}>Progresive</StyledButton>
+        </StyledContainer>      
+      
     </div>
   );
 }
